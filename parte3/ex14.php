5e473483bc29025851d24a749ef54aa5a6e54c44
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2025-10-05 19:45:46 -0300

    exercicios

---
+++ parte3/ex14.php
@@ -0,0 +1,10 @@
+<?php
+// Definindo um número hexadecimal
+$hex = 0x1A; // 1A é o valor hexadecimal
+
+// Convertendo para decimal
+$decimal = hexdec($hex);
+
+echo "Número hexadecimal: " . dechex($hex) . "\n"; // Imprime o número hexadecimal
+echo "Número em decimal: " . $decimal . "\n"; // Imprime o valor em decimal
+?>
---
+++ parte3/ex15.php
@@ -0,0 +1,10 @@
+<?php
+// Definindo um número octal
+$octal = 017; // 017 é o valor octal
+
+// Convertendo para decimal
+$decimal = octdec($octal);
+
+echo "Número octal: " . decoct($octal) . "\n"; // Imprime o número octal
+echo "Número em decimal: " . $decimal . "\n"; // Imprime o valor em decimal
+?>
